165ac173234c01372717ce6860db391b65cae835
janvi <janvi@cloudmatetechnologies.com>
2023-03-14 14:44:17 +0530

    refactor: added navbar an footer component

--- user-portal/src/components/molecules/AvailabilityFilter.jsx
+++ user-portal/src/components/molecules/AvailabilityFilter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AvailabilityFilter() {
   return (
     <div class="filter-option">
-    <span class="filter-name m-2"><b>AVAILABILITY</b></span>
+    <span class="filter-name m-2"><b>AVAILABILITY</b></span><br/>
     <input type="checkbox" class="mx-2 mb-3" /> Include Out of Stock<br/>
 </div>
   )
--- user-portal/src/components/pages/ProductGallary.jsx
+++ user-portal/src/components/pages/ProductGallary.jsx
@@ -3,19 +3,23 @@ import FilterOptions from '../organisms/FilterOptions'
 import ProductList from '../organisms/ProductList'
 import ShortAndViewOptions from '../organisms/ShortAndViewOption'
 import Footer from '../organisms/Footer'
+import MainNavbar from '../organisms/MainNavbar'
 
 function ProductGallary() {
   return (
-    <div class="container-fluid w-100">
-    <div class="main_section d-flex" >
-        <FilterOptions />
-        <div class="product_section container-fluid">
+    <>
+      <div class=" w-100">
+      <MainNavbar />
+        <div class="main_section d-flex" >
+          <FilterOptions />
+          <div class="product_section container-fluid">
             <ShortAndViewOptions />
             <ProductList />
+          </div>
         </div>
+      <Footer className="footer d-flex align-items-center" />
       </div>
-        <Footer className="footer d-flex align-items-center" />
-    </div>
+    </>
   )
 }
 
