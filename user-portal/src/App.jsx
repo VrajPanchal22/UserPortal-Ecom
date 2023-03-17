102cb1da9c0cce437e0b1784c18d81a2b46bdbea
Sonali Prajapati <sonali@cloudmatetechnologies.com>
2023-03-17 16:33:30 +0530

    feature: Add Home page

--- user-portal/src/App.jsx
+++ user-portal/src/App.jsx
@@ -1,23 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
- import Login from './components/organisms/Login';
+import Login from './components/organisms/Login';
 import Signup from './components/organisms/Signup';
-import Header from './components/organisms/Header';
-import MainNavbar from './components/organisms/MainNavbar'
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    // <BrowserRouter>
-    // <Routes>
-    //   <Route path='/' element={<Login />}></Route>
-    //   <Route path='/signup' element={<Signup />}></Route>
-    // </Routes>
-    // </BrowserRouter>
-  <>
-  <MainNavbar />
-  <Header />
-  
-  </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+      </Routes>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- user-portal/src/components/organisms/MainNavbar.jsx
+++ user-portal/src/components/organisms/MainNavbar.jsx
@@ -10,10 +10,17 @@ import {
   Nav,
 
 } from 'reactstrap';
+import FlyOut from '../molecules/FlyOut'
 
 function MainNavbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+
+  const [isFlyoutToggle, setIsFlyoutToggle] = useState(false)
+  const handleClick = () => {
+    console.log("hellos")
+    setIsFlyoutToggle(!isFlyoutToggle)
+  }
   return (
     <div className="main-header navbar navbar-expand-md" id="topnav">
       <div className="main-logo d-flex justify-content-center align-items-center mr-2">
@@ -22,17 +29,18 @@ function MainNavbar() {
       <SearchField />
       <div className="responsive">
         <div className='on-responsive'>
-        <div className="button shadow">
-          <Button className="login-button" buttonText="Login" />
+          <div className="button shadow">
+            <Button className="login-button" buttonText="Login" />
+          </div>
+          <NavbarToggler className='bg-light hamburger' onClick={toggle} />
         </div>
-        <NavbarToggler className='bg-light hamburger' onClick={toggle} />
-        </div> 
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
-            <NavbarMenus />
+            <NavbarMenus handleClick={handleClick} />
           </Nav>
         </Collapse>
       </div>
+      {isFlyoutToggle? <FlyOut handleClick={handleClick} /> : null}
     </div>
   )
 }
