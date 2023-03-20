6e2f0cc4ae4d5d52b01b260f4b6d8ec97cb6e8bd
Sonali Prajapati <sonali@cloudmatetechnologies.com>
2023-03-20 11:20:12 +0530

    feature: add search functionality

--- user-portal/src/components/organisms/MainNavbar.jsx
+++ user-portal/src/components/organisms/MainNavbar.jsx
@@ -3,30 +3,37 @@ import Button from '../atoms/Button'
 import ImgTag from '../atoms/ImgTag'
 import NavbarMenus from '../molecules/NavbarMenus'
 import SearchField from '../molecules/SearchField'
-
 import {
   Collapse,
   NavbarToggler,
   Nav,
-
 } from 'reactstrap';
 import FlyOut from '../molecules/FlyOut'
+import { useParams } from 'react-router-dom'
 
 function MainNavbar() {
+  const [inputSearch, setInputSearch] = useState('')
+  const { name } = useParams();
+
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
   const [isFlyoutToggle, setIsFlyoutToggle] = useState(false)
   const handleClick = () => {
-    console.log("hellos")
     setIsFlyoutToggle(!isFlyoutToggle)
   }
+
   return (
     <div className="main-header navbar navbar-expand-md" id="topnav">
       <div className="main-logo d-flex justify-content-center align-items-center mr-2">
         <ImgTag imgUrl='/assets/images/logo1.png' width="110" alt='logo' />
       </div>
-      <SearchField />
+      <SearchField type="text"
+        placeholder="Search for products brand & more.."
+        id="category"
+        value={inputSearch}
+        onChange={(e) => setInputSearch(e.target.value)}
+      />
       <div className="responsive">
         <div className='on-responsive'>
           <div className="button shadow">
@@ -40,7 +47,7 @@ function MainNavbar() {
           </Nav>
         </Collapse>
       </div>
-      {isFlyoutToggle? <FlyOut handleClick={handleClick} /> : null}
+      {isFlyoutToggle ? <FlyOut handleClick={handleClick} /> : null}
     </div>
   )
 }
